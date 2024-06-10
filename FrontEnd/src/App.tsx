32592f2ceeb31733fe8b4a01c96d0ca3e7ade126
MickeMarten <KarlMikael.Martensson@gmail.com>
2024-06-10 18:27:03 +0200

    Luxon

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,7 +1,20 @@
+import '@ionic/react/css/core.css';
+import { IonCardHeader, IonCardSubtitle, IonCardTitle, IonChip, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonImg, IonInput, IonProgressBar, IonRow, IonSearchbar, IonText, IonToolbar, setupIonicReact } from '@ionic/react';
+import { IonApp, IonRouterOutlet, IonCard, IonCardContent } from '@ionic/react';
+import { useEffect } from 'react';
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getFirestore, collection, addDoc, getDocs, doc } from "firebase/firestore";
 import { useState } from "react";
+import { DateTime, Duration } from "luxon";
 import './App.css'
+setupIonicReact();
+import { IonButton, IonDatetime } from '@ionic/react';
+
+interface Workday {
+  workHours: number,
+  workSeconds: number,
+  date: string,
+}
 
 const firebaseConfig = {
   apiKey: "AIzaSyB7GnC4ffCPkTW7kGhedZMAX5_LSnvoUpk",
@@ -17,57 +30,98 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// const docRef = await addDoc(collection(db, 'Timestamps'), {
-  
-// })
+
+
+
 
 function Page() {
-let time:String = new Date().toLocaleTimeString()
-let date:String = new Date().toLocaleDateString()
-const [realTime, setRealTime] = useState(time)
+  const [startWork, setStartWork] = useState(0)
+  const [toggleBtn, setToggleBtn] = useState(false)
+  const [workData, setWorkData] = useState([])
+  const today = DateTime.now().toLocaleString();
 
-const updateTime = () =>{
-  time = new Date().toLocaleTimeString();
-  setRealTime(time);
-}
-setInterval(updateTime);
 
-// async function startWork() {
-//   try {
-//   const docRef = await addDoc(collection(db, "clockIn"), {
-//     date: date,
-//     time: time
-//   });
 
-//   console.log("Document written with ID: ", docRef.id);
-// } catch (e) {
-//   console.error("Error adding document: ", e);
-// }
-// }
+
+  function startBtn() {
+    const startTimer = DateTime.now().second
+    setStartWork(startTimer);
+    setToggleBtn(true)
+
+
+  }
+
+  function stopBtn() {
+    const stopWork = DateTime.now().second;
+    const workTime = Duration.
+    const hoursWorked = workTime.minus(stopWork)
+
+
+    setToggleBtn(false);
+
+    const docRef = addDoc(collection(db, 'clockIn'), {
+
+      workHours: hoursWorked,
+      date: today
+    });
+
+  }
+
+  useEffect(() => {
+    async function getData() {
+      const dataList: Workday[] = []
+
+      const querySnapshot = await getDocs(collection(db, 'clockIn'));
+      querySnapshot.forEach((doc) => {
+        dataList.push(doc.data())
+      })
+      setWorkData(dataList)
+
+    }
+    getData()
+
+  }, [])
+
 
   return (
-    <main> 
-<h1 className=''>Dags att stämpla in</h1>
-<p>Idag är det {date} klockan {realTime}</p>
+    <IonApp>
+      <IonHeader>
+        <IonToolbar >
+          <IonText>Välkommen till jobbet. Idag är det {today}</IonText>
+          <IonInput label='Namn' placeholder='Ditt namn här idiot'></IonInput>
+          <IonButton onClick={startBtn} style={{ display: toggleBtn ? 'none' : '' }}>Börja jobba</IonButton>
+          <IonButton onClick={stopBtn} style={{ display: toggleBtn ? '' : 'none' }}>Sluta jobba</IonButton>
 
-<div className="max-w-36 bg-red-500"> 
-<div className="border border-black text-center" onClick={async function startWork() {
-  try {
-  const docRef = await addDoc(collection(db, "clockIn"), {
-    date: date,
-    time: time
-  });
+          <IonSearchbar color="light" placeholder='Sök här' slot=''></IonSearchbar>
+        </IonToolbar>
+      </IonHeader>
 
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
-}}>
-  börja jobba
-</div>
-</div>
+      <IonContent>
+        <IonGrid>
+          <IonRow>
 
-</main>
+            {workData.map((item) => (
+              <IonCard>
+                <IonCardContent>
+                  <IonCol>
+                    <IonCardSubtitle>{item.date}</IonCardSubtitle>
+                  </IonCol>
+                  <IonCol>
+                    <IonCardSubtitle>{item.workHours} timmar och  {item.workSeconds} minuter arbetade</IonCardSubtitle>
+                  </IonCol>
+
+                </IonCardContent>
+              </IonCard>
+
+            ))}
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+
+
+    </IonApp >
+
+
   )
 }
 
